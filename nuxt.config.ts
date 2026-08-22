cd7d87735425686b0ce26a437c0c3959deb87bba
Olaf Cazarez <olafc@zoomcatalog.com>
2026-08-22 16:16:00 -0700

    ! Add metadata support for facebook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,63 @@ export default defineNuxtConfig({
     '~/assets/css/main.css'
   ],
 
+  app: {
+    head: {
+      title: 'Café 88 y Faja de Oro',
+
+      meta: [
+        {
+          name: 'description',
+          content: 'Café veracruzano de altura y productos gourmet elaborados con tradición, calidad y pasión.'
+        },
+
+        {
+          property: 'og:title',
+          content: 'Café 88 y Faja de Oro'
+        },
+        {
+          property: 'og:description',
+          content: 'Café veracruzano de altura y productos gourmet elaborados con tradición, calidad y pasión.'
+        },
+        {
+          property: 'og:type',
+          content: 'website'
+        },
+        {
+          property: 'og:url',
+          content: 'https://cafe88.mx/'
+        },
+        {
+          property: 'og:image',
+          content: 'https://cafe88.mx/images/social/cafe88.png'
+        },
+        {
+          property: 'og:image:width',
+          content: '512'
+        },
+        {
+          property: 'og:image:height',
+          content: '512'
+        },
+        {
+          property: 'og:image:alt',
+          content: 'Café 88'
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary_large_image'
+        }
+      ],
+
+      link: [
+        {
+          rel: 'canonical',
+          href: 'https://cafe88.mx/'
+        }
+      ]
+    }
+  },
+
   runtimeConfig: {
     erpBaseUrl: process.env.ERP_BASE_URL || 'http://erp.cafe88.org',
     erpApiToken: process.env.ERP_API_TOKEN || '',
